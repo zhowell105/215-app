9c2f3af8ae0983cfdf131d24e7d345870359aa57
Zoe' Howell <zhowell105@andersonuniversity.edu>
2024-09-18 13:33:03 +0000

    profile and other pages

---
+++ src/app/interests/interests.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interests',
+  standalone: true,
+  imports: [],
+  templateUrl: './interests.component.html',
+  styleUrl: './interests.component.css'
+})
+export class InterestsComponent {
+
+}
